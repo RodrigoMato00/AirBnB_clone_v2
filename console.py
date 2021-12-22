b8af76eaa8c8fbe6ddda39dbaaf2acc6e877f985
RodrigoMato00 <3293@holbertonschool.com>
2021-12-22 06:30:30 -0800

    pycodestyle

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -140,9 +140,10 @@ class HBNBCommand(cmd.Cmd):
             obj.save()
             print("{}".format(obj.id))
         except SyntaxError:
-           pass
+            pass
         except NameError:
             pass
+
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
@@ -336,6 +337,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
