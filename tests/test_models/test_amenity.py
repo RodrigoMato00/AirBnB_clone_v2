569c39a0c19cd9e6d848f11e6e271bdbd64046e0
RodrigoMato00 <3293@holbertonschool.com>
2021-12-23 09:47:31 -0800

    style

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+UNITEST
+"""
+
+import models
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
 from models.amenity import Amenity
+import pep8
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(unittest.TestCase):
+    """
+    Test for amenity
+    """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    def test_amenity(self):
+        """
+        amenity
+        """
+        a = Amenity(name="a")
+        self.assertEqual(str, type(a.name))
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_pep8(self):
+        """
+        style
+        """
+        i = pep8.StyleGuide(quiet=True)
+        style = i.check_files(['models/amenity.py'])
+        self.assertEqual(style.total_errors, 0, 'Found errors.')
+
+
+if _name_ == '_main_':
+    unittest.main()
