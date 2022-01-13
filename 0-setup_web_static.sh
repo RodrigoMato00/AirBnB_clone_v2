abac3e8683487f6be234dc970196e001ccf99dbe
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2022-01-13 16:29:11 -0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # all all all all
 
-apt-get update -y
-apt-get install nginx -y
-ufw allow 'Nginx HTTP'
+apt update
+apt install nginx -y
 mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
 sudo echo "<html>
@@ -14,7 +13,7 @@ sudo echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu /data
-sudo chgrp -R ubuntu /data
+chown -R ubuntu /data
+chgrp -R ubuntu /data
 sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
 service nginx restart
