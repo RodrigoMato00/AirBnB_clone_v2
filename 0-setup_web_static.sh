9f87ed65e5f5e2f6027cca2bbe2a119dce688769
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2022-01-13 16:32:23 -0300

    0 funciona

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,16 +3,16 @@
 
 apt update
 apt install nginx -y
-mkdir -p /data/web_static/shared
-mkdir -p /data/web_static/releases/test
-sudo echo "<html>
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data
 chgrp -R ubuntu /data
 sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
