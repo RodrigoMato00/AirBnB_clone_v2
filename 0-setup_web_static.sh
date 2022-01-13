f50ac00caad4643138a390a6788baf5cb628d992
RodrigoMato00 <3293@holbertonschool.com>
2022-01-13 10:58:39 -0800

    hola

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # all all all all
 
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
+apt-get update -y
+apt-get install nginx -y
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
 sudo chown -R ubuntu /data
 sudo chgrp -R ubuntu /data
 sudo echo "<html>
@@ -14,6 +15,6 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
+ln -sf /data/web_static/current /data/web_static/releases/test/
+sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
 service nginx restart
