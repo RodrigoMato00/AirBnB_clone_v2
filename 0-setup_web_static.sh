1b079516b9f7a4b3fbea223e9d2219d363ed6bb4
RodrigoMato00 <3293@holbertonschool.com>
2022-01-13 11:21:38 -0800

    para vos

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,6 @@ apt-get install nginx -y
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test
-sudo chown -R ubuntu /data
-sudo chgrp -R ubuntu /data
 sudo echo "<html>
   <head>
   </head>
@@ -16,5 +14,7 @@ sudo echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu /data
+sudo chgrp -R ubuntu /data
 sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/; }" /etc/nginx/sites-available/default
 service nginx restart
