c8eb0eaee951ee6ef32eb09550fb6655103d5f68
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 19:10:57 -0300

    correccion 1000

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,26 +6,26 @@ module script that starts Flask
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
+@app.route('/cities_by_states', strict_slashes=False)
+def display_states():
     """
     display a HTML page
     list of all states and related cities
     """
-    return render_template("8-cities_by_states.html", states=states)
+    storage_states = storage.all('State')
+    return render_template('8-cities_by_states.html', storage_states=states)
 
 
 @app.teardown_appcontext
-def teardown_close(self):
-    """
-    method teardown
-    """
+def teardown(self):
+    """ close storage """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000, debug=True)
