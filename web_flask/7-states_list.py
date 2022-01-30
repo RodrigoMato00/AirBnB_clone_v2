d1931d8665439b031d7d9e0909138974b244c828
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 14:32:45 -0300

    db

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present
+in DBStorage sorted by name (A->Z) tip
+LI tag: description of one State:
+<state.id>: <B><state.name></B>
+"""
