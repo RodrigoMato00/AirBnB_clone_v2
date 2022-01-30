6fde1b50bbefdf3b9ffbd49a1a5330304b859c7c
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 15:54:59 -0300

    user

--- models/user.py
+++ models/user.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""
+this module defines a class User
+"""
+
 from models.review import Review
 from models.place import Place
 from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """
+    defines a user by various attributes
+    """
     __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
