1adbda9d7168d535d1caaa7eaedb2dc585d662ec
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 15:47:06 -0300

    base  y amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""
+State Module for HBNB projec
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -7,7 +9,9 @@ from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """ Class Amenity """
+    """
+    Class Amenity
+    """
     __tablename__ = 'amenities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""
+This module defines a base class for all models in our hbnb clone
+"""
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
@@ -9,14 +11,18 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """
+    base class for all hbnb models
+    """
 
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(),  nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """
+        Instatntiates a new model
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -39,21 +45,26 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """Returns a string representation of the instance"""
+        """
+        Returns a string representation of the instance
+        """
         dictionary = self.to_dict()
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, dictionary)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """
+        Updates updated_at with current time when instance is changed
+        """
         from models import storage
         self.updated_at = datetime.now()
-        # storage.new(self) moved from __init__ before storage.save()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
+        """
+        Convert instance into dict format
+        """
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
@@ -65,7 +76,9 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        """ delete the current instance from the storage
-        (models.storage) by calling the method delete """
+        """
+        delete the current instance from the storage
+        (models.storage) by calling the method delete
+        """
         from models import storage
         storage.delete(self)
