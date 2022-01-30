4bb3921ee8b4a529f06d8e397fa7f96e6f575c9a
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 16:23:34 -0300

    7 value agregado

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,16 +17,16 @@ app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def states_ls():
+def states_lists():
     """
     state list
     """
     states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return render_template("7-states_list.html", states=states.all("state").values())
 
 
 @app.teardown_appcontext
-def handle_trd():
+def teardown_close():
     """
     method teardown
     """
