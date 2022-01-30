40927fc1032b77f5a7af44d6d6e08254b7c8e49c
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 12:26:36 -0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+from models import storage
 
 Base = declarative_base()
 
@@ -18,25 +19,31 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if kwargs:
-            if "id" not in kwargs.keys():
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs.keys():
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs.keys():
-                self.updated_at = datetime.now()
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        dict = self.to_dict()
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -47,13 +54,15 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        my_dict = dict(self.__dict__)
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        if "_sa_instance_state" in my_dict.keys():
-            del my_dict["_sa_instance_state"]
-        return my_dict
+        dict = {}
+        dict.update(self.__dict__)
+        dict.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dict.keys():
+            dict.pop('_sa_instance_state')
+        return dict
 
     def delete(self):
         """deletes thingies"""
