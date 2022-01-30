410d6f911fd2a1d1e9a423a5c1a0298273c87531
RodrigoMato00 <3293@holbertonschool.com>
2022-01-30 14:53:17 -0300

    8 probando

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,3 +7,31 @@ in DBStorage sorted by name (A->Z) tip
 LI tag: description of one State:
 <state.id>: <B><state.name></B>
 """
+
+from flask import Flask
+from flask import render_template
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_ls():
+    """
+    state list
+    """
+    st = storage.all("State").values()
+    return render_template("7-states_list.html", states=st)
+
+
+@app.teardown_appcontext
+def handle_trd(self):
+    """
+    method teardown
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
