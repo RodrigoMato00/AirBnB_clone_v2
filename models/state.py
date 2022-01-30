0bee695980915635408e65ef893a95b7527ace9d
RodrigoMato00 <3293@holbertonschool.com>
2022-01-29 22:27:35 -0300

    correcciones

--- models/state.py
+++ models/state.py
@@ -12,15 +12,13 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all,
+                              delete-orphan')
     else:
         @property
         def cities(self):
             """ FileStorage relationship between State and City. """
-            ret = []
-            for key, value in models.storage.all(City).items():
-                if value.state_id == self.id:
-                    ret.append(value)
-            return ret
+        return [city for city in models.storage.all(City).values()
+                if city.state_id == self.id]
