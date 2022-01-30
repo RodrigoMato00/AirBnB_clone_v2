51645cc4e7816d015760773cc887d391152dd1da
RodrigoMato00 <3293@holbertonschool.com>
2022-01-29 22:59:34 -0300

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""
+This module defines a class to manage file storage for hbnb clone
+"""
 import json
+import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -11,35 +14,43 @@ from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """
+    This class manages storage of hbnb models in JSON format
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-        if cls is not None:
-            ndict = {}
-            for k, v in self.__objects.items():
-                if cls == type(v):
-                    ndict[k] = v
-            return ndict
+        """
+        Returns a dictionary of models currently in storage
+        """
+        if cls:
+            return {key: obj for (key, obj) in self.__objects.items()
+                    if isinstance(obj, cls)}
         return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        """
+        Adds new object to storage dictionary
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """
+        Saves storage dictionary to file
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """
+        Loads storage dictionary from file
+        """
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
                 for key, value in (json.load(f)).items():
@@ -49,15 +60,74 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ delete obj from __objects if it’s inside"""
-        if obj is not None:
-            k = obj.__class__.__name__ + '.' + obj.id
-            if k in self.__objects.keys():
-                del self.__objects[k]
-            self.save()
+        """
+        delete obj from __objects if it’s inside
+        """
+        if obj in self.__objects.values():
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del(self.__objects[key])
+
+    def classes(self):
+        """
+        Returns avalid classes and the references
+        """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel, "User": User,
+                   "State": State, "City": City,
+                   "Amenity": Amenity, "Place": Place,
+                   "Review": Review}
+        return classes
+
+    def attributes(self):
+        """
+        Returns valid attributes and the types
+        for classname
+        """
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
 
     def close(self):
         """
         Call a reload method
         """
-        self.reload()
+        return self.reload()
